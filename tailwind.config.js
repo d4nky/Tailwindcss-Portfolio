ed11c2a6e59b9af73cf4a902b482c58c9e84b780
d4nky <vertix08@gmail.com>
2021-12-31 22:14:09 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./dist/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Roboto', 'Helvetica', 'Arial', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
