2628500dd3d77119b6d0769a86b12bfd05db5fd6
d4nky <vertix08@gmail.com>
2022-01-02 00:04:16 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,17 @@ module.exports = {
     extend: {
       fontFamily: {
         'custom': ['Montserrat', 'mono'],
+      },
+      keyframes: {
+        typewriter: {
+          'from': {width: '0'},
+          'to' : {width: '100%'},
       }
     },
+    animation: {
+        typewriter: 'typewriter 1.5s steps(50, end) forwards',
+      }
+    }, 
   },
   plugins: [],
 }
