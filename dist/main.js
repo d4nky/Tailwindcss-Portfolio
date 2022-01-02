76ab38143c6f90a47fc86ace93f952939808dd6c
d4nky <vertix08@gmail.com>
2022-01-02 18:56:45 +0100

    update

--- dist/main.js
+++ dist/main.js
@@ -7,3 +7,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const navbar = document.querySelector('nav');
+const navbarActive = document.getElementById('navbar');
+const headerTitle = document.getElementById('header-title');
+const offsetY = 50;
+
+window.addEventListener('scroll', (e) =>{
+    console.log(window.pageYOffset)
+    if (window.pageYOffset > offsetY){
+        navbar.style.backgroundColor = 'black';
+        navbar.style.paddingTop = '38px';
+        navbar.style.paddingBottom = '38px';
+    }
+    else if(window.pageYOffset < offsetY){
+        navbar.style.backgroundColor = 'transparent';
+        navbar.style.paddingTop = '32px';
+        navbar.style.paddingBottom = '32px';
+    }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,25 @@ module.exports = {
         typewriter: {
           'from': {width: '0'},
           'to' : {width: '100%'},
+      },
+      blink: {
+        'from': {borderColor: 'transparent'},
+        'to': {borderColor: 'white'},
+      },
+      nav: {
+        '0%': {opacity: '0',
+        marginTop: '-200px'
+      },
+        '100%': {opacity: '1',
+        marginTop: '0px'
+      },
       }
     },
     animation: {
-        typewriter: 'typewriter 2.5s steps(200, end) forwards',
+        typewriter: 'typewriter 12s forwards, blink .5s infinite',
+        nav: 'nav .5s forwards',
       }
+      
     }, 
   },
   plugins: [],
