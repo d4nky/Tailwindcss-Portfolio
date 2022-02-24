74cf5f50fed9915f371c9dd34fc1bc0c8a76ce73
Fares <kherbanefares@gmail.com>
2022-02-24 15:49:11 +0100

    fixed text, picture and margins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 module.exports = {
 	content: ["./dist/*.html"],
 	theme: {
+		fontSize: {
+			xs: ".75rem",
+			sm: ".875rem",
+			tiny: ".875rem",
+			base: "1rem",
+			lg: "1.125rem",
+			xl: "1.25rem",
+			"2xl": "1.5rem",
+			"3xl": "1.875rem",
+			"4xl": "2.25rem",
+			"5xl": "3rem",
+			"6xl": "4rem",
+			"7xl": "5rem",
+			"1vw": "10vw",
+			"2vw": "20vw",
+			"3vw": "30vw",
+		},
 		screens: {
 			sm: "640px",
 			// => @media (min-width: 640px) { ... }
