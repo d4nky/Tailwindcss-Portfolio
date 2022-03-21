bfff8fb2ddb9a869314205d89dcb9347e0134d05
Fares <kherbanefares@gmail.com>
2022-03-21 23:39:48 +0100

    Revert "added work section content"
    This reverts commit e3cc6134b2f47a7c3c72e24ed18b3a2ebfe3ebf4.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,37 @@
 module.exports = {
-	content: ['./dist/*.html'],
+	content: ["./dist/*.html"],
 	theme: {
 		fontSize: {
-			xs: '.75rem',
-			sm: '.875rem',
-			tiny: '.875rem',
-			base: '1rem',
-			lg: '1.125rem',
-			xl: '1.25rem',
-			'2xl': '1.5rem',
-			'3xl': '1.875rem',
-			'4xl': '2.25rem',
-			'5xl': '3rem',
-			'6xl': '4rem',
-			'7xl': '5rem',
-			'1vw': '10vw',
-			'2vw': '20vw',
-			'3vw': '30vw',
+			xs: ".75rem",
+			sm: ".875rem",
+			tiny: ".875rem",
+			base: "1rem",
+			lg: "1.125rem",
+			xl: "1.25rem",
+			"2xl": "1.5rem",
+			"3xl": "1.875rem",
+			"4xl": "2.25rem",
+			"5xl": "3rem",
+			"6xl": "4rem",
+			"7xl": "5rem",
+			"1vw": "10vw",
+			"2vw": "20vw",
+			"3vw": "30vw",
 		},
 		screens: {
-			sm: '640px',
+			sm: "640px",
 			// => @media (min-width: 640px) { ... }
 
-			md: '768px',
+			md: "768px",
 			// => @media (min-width: 768px) { ... }
 
-			lg: '1024px',
+			lg: "1024px",
 			// => @media (min-width: 1024px) { ... }
 
-			xl: '1280px',
+			xl: "1280px",
 			// => @media (min-width: 1280px) { ... }
 
-			'2xl': '1536px',
+			"2xl": "1536px",
 			// => @media (min-width: 1536px) { ... }
 		},
 		extend: {
@@ -40,26 +40,26 @@ module.exports = {
 				profile: "url('img/about_pic.jfif')",
 			},
 			fontFamily: {
-				custom: ['Montserrat', 'mono'],
+				custom: ["Montserrat", "mono"],
 			},
 			keyframes: {
 				typewriter: {
-					from: { width: '0' },
-					to: { width: '100%' },
+					from: { width: "0" },
+					to: { width: "100%" },
 				},
 				blink: {
-					from: { borderColor: 'transparent' },
-					to: { borderColor: 'white' },
+					from: { borderColor: "transparent" },
+					to: { borderColor: "white" },
 				},
 				nav: {
-					'0%': { opacity: '0', marginTop: '-200px' },
-					'100%': { opacity: '1', marginTop: '0px' },
+					"0%": { opacity: "0", marginTop: "-200px" },
+					"100%": { opacity: "1", marginTop: "0px" },
 				},
 			},
 			animation: {
 				typewriter:
-					'typewriter 3s steps(120, end) forwards, blink .5s infinite',
-				nav: 'nav 1s forwards',
+					"typewriter 3s steps(120, end) forwards, blink .5s infinite",
+				nav: "nav 1s forwards",
 			},
 		},
 	},
